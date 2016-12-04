2500f13e9ea7453f8461680302f6a12cf2b2694e
Hugo Delavenne <avatarsblanc@gmail.com>
2016-12-04 23:14:24 +0100

    J'ai renommé data en donnee et getBytes en getOctets.

--- BitTampon.cpp
+++ BitTampon.cpp
@@ -3,7 +3,7 @@
 
 //Constructeur
 QAmiensRCodeGeneration::BitTampon::BitTampon() :
-	data(),
+	donnee(),
 	bitLongueur(0) {}
 
 
@@ -12,8 +12,8 @@ int QAmiensRCodeGeneration::BitTampon::obtenirLongueurBit() const {
 }
 
 
-std::vector<uint8_t> QAmiensRCodeGeneration::BitTampon::getBytes() const {
-	return data;
+std::vector<uint8_t> QAmiensRCodeGeneration::BitTampon::getOctets() const {
+	return donnee;
 }
 
 
@@ -21,19 +21,19 @@ void QAmiensRCodeGeneration::BitTampon::ajouterBits(uint32_t val, int longueur) 
 	if (longueur < 0 || longueur > 32 || (longueur < 32 && (val >> longueur) != 0))
 		throw "Valeur en dehors de la plage définie";
 	size_t newBitLen = bitLongueur + longueur;
-	while (data.size() * 8 < newBitLen)
-		data.push_back(0);
+	while (donnee.size() * 8 < newBitLen)
+		donnee.push_back(0);
 	for (int i = longueur - 1; i >= 0; i--, bitLongueur++)  // Ajout bit par bit
-		data.at(bitLongueur >> 3) |= ((val >> i) & 1) << (7 - (bitLongueur & 7));
+		donnee.at(bitLongueur >> 3) |= ((val >> i) & 1) << (7 - (bitLongueur & 7));
 }
 
 
 void QAmiensRCodeGeneration::BitTampon::ajouterDonnees(const QrSegment &seg) {
 	size_t newBitLen = bitLongueur + seg.bitLength;
-	while (data.size() * 8 < newBitLen)
-		data.push_back(0);
+	while (donnee.size() * 8 < newBitLen)
+		donnee.push_back(0);
 	for (int i = 0; i < seg.bitLength; i++, bitLongueur++) {  // Ajout bit par bit
-		int bit = (seg.data.at(i >> 3) >> (7 - (i & 7))) & 1;
-		data.at(bitLongueur >> 3) |= bit << (7 - (bitLongueur & 7));
+		int bit = (seg.donnee.at(i >> 3) >> (7 - (i & 7))) & 1;
+		donnee.at(bitLongueur >> 3) |= bit << (7 - (bitLongueur & 7));
 	}
 }
--- BitTampon.hpp
+++ BitTampon.hpp
@@ -7,46 +7,46 @@
 
 namespace QAmiensRCodeGeneration {
 
-/* 
+/*
  * Une suite de bits accessibles. Les bits sont organisés en 'gros-boutien" (ou 'big-endian' : https://fr.wikipedia.org/wiki/Endianness) dans un octet.
  */
 class BitTampon final {
-	
+
 	/*---- Champs----*/
 private:
-	
-	std::vector<uint8_t> data;
+
+	std::vector<uint8_t> donnee;
 	int bitLongueur;
-	
-	
-	
+
+
+
 	/*---- Constructeur ----*/
 public:
-	
+
 	// Creates an empty bit buffer (length 0).
 	BitTampon();
-	
-	
-	
+
+
+
 	/*---- Méthodes ----*/
 public:
-	
+
 	// Renvoie le nombre de bits dans le tampon, qui est une valeur positive ou nulle.
 	int obtenirLongueurBit() const;
-	
-	
+
+
 	// Retourne une copie de tous les octets, et remplissage jusqu'à l'octet le plus proche.
-	std::vector<uint8_t> getBytes() const;
-	
-	
+	std::vector<uint8_t> getOctets() const;
+
+
 	// Ajoute le nombre donné de bits de la valeur donnée à cette séquence.
 	// Si 0 ≤ len ≤ 31, alors le nombre donné de bits vaut 0 ≤ val < 2^(longueur).
 	void ajouterBits(uint32_t val, int longueur);
-	
-	
+
+
 	// Ajoute les données du segment donné à ce tampon de bits.
 	void ajouterDonnees(const QrSegment &seg);
-	
+
 };
 
 }
