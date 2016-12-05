dc0a28499f6942b52abd259556786e01b6bb1764
AvatarsBlanc <avatarsblanc@gmail.com>
2016-12-05 14:19:42 +0100

    Fin de la traduction de QAmiensRSegment.hpp

--- QAmiensRSegment.hpp
+++ QAmiensRSegment.hpp
@@ -76,15 +76,11 @@ public:
 	// Fonctions statiques du helper
 public:
 
-	/*
-	 * Tests whether the given string can be encoded as a segment in alphanumeric mode.
-	 */
+	// Teste si on peut encoder en alphanumérique
 	static bool isAlphanumeric(const char *text);
 
 
-	/*
-	 * Tests whether the given string can be encoded as a segment in numeric mode.
-	 */
+	// Teste si on peut encoder en numérique
 	static bool isNumeric(const char *text);
 
 
@@ -92,36 +88,31 @@ public:
 	// Champs d'instance
 public:
 
-	/* The mode indicator for this segment. */
+	// Mode de la séquence
 	const Mode mode;
 
-	/* The length of this segment's unencoded data, measured in characters. Always zero or positive. */
+	// Nombre de caractères
 	const int numChars;
 
-	/* The bits of this segment packed into a byte array in big endian. */
+	// Séquence en bits
 	const std::vector<uint8_t> donnee;
 
-	/* The length of this segment's encoded data, measured in bits. Satisfies ceil(bitLength / 8) = data.size(). */
+	// Nombre de bits
 	const int bitLength;
 
 
 	// Constructeur
 public:
 
-	/*
-	 * Creates a new QR Code data segment with the given parameters and data.
-	 */
     QAmiensRSegment(const Mode &md, int numCh, const std::vector<uint8_t> &b, int bitLen);
 
-
-	// Package-private helper function.
 	static int getTotalBits(const std::vector<QAmiensRSegment> &segs, int version);
 
 
-	/*---- Private constant ----*/
+	// Petite constante privée oklm
 private:
 
-	/* Maps shifted ASCII codes to alphanumeric mode character codes. */
+	// Table ASCII des caractères numériques
 	static const int8_t ALPHANUMERIC_ENCODING_TABLE[59];
 
 };
